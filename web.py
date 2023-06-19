d4521de58aa08a5ed2bd74849de4a7ba69efadc2
DwaraYogaP <75575434+DwaraYogaP@users.noreply.github.com>
2023-06-19 23:50:01 +0700

    Add files via upload

---
+++ web.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+
+def rf_predict(data):
+  if data == 0:
+    return "not_cyberbullying"
+  elif data == 1:
+    return "ethnicity"
+  elif data == 2:
+    return "gender"
+  elif data == 3:
+    return "age"
+  else:
+    return "religion"
+
+data = pd.read_csv('cyberbullying_tweets.csv')
+df = pd.DataFrame(data)
+
+
+
+st.title("PREDIKSI KLASIFIKASI CYBERBULLYING")
+
+st.write("Cyberbulyying Dataset")
+st.dataframe(data)
+
+model = joblib.load("model_rf.pkl")
+vector = joblib.load("vectorizer.pkl")
+
+with st.form("nlpForm"):
+    text = st.text_area("Masukkan Text Disini")
+    submit_button = st.form_submit_button(label='Proses')
+    
+    col1,col2 = st.columns(2)
+    if submit_button:
+        
+        with col1:
+
+            st.info("Hasil")
+        
+            vector_text = vector.transform([text])
+            hasil = model.predict(vector_text)
+            kategori = rf_predict(hasil)
+        with col2:
+            st.success(kategori)
